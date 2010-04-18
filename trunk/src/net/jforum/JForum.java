e2ce5613d5850119d3688bb6f1009b9afdadac8e
zhurx4g <zhurx4g@a3948604-d997-11de-b113-f7137c4feea6>
2010-04-18 01:46:45 +0000

    修正错误页面乱码。

--- trunk/src/net/jforum/JForum.java
+++ trunk/src/net/jforum/JForum.java
@@ -300,7 +300,7 @@ public class JForum extends JForumBaseServlet
 				new ExceptionWriter().handleExceptionData(e, out, request);
 			}
 			else {
-				new ExceptionWriter().handleExceptionData(e, new BufferedWriter(new OutputStreamWriter(response.getOutputStream())), request);
+				new ExceptionWriter().handleExceptionData(e, new BufferedWriter(new OutputStreamWriter(response.getOutputStream(),encoding)), request);
 			}
 		}
 	}
