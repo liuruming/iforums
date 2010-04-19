12e24dd8c27c692231854f9d8721ad7443da12af
zhurx4g <zhurx4g@a3948604-d997-11de-b113-f7137c4feea6>
2010-04-19 06:10:02 +0000

    解决 JForum 搜索错误的问题

--- src/net/jforum/search/LuceneSearch.java
+++ src/net/jforum/search/LuceneSearch.java
@@ -159,7 +159,8 @@ public class LuceneSearch implements NewDocumentAdded
 			}
 		}
 		catch (Exception e) {
-			throw new SearchException(e);
+			logger.error("",e);
+			result = new SearchResult(new ArrayList(), 0);
 		}
 		
 		return result;
