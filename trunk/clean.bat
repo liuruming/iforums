abc9358d4b65cca2798bc1580d69689e911da555
zhurx4g@gmail.com <zhurx4g@gmail.com@a3948604-d997-11de-b113-f7137c4feea6>
2012-04-23 11:22:22 +0000

    init

---
+++ trunk/clean.bat
@@ -0,0 +1,4 @@
+cd E:\home\workspace\iforums
+rm -r D:\software\Tomcat-6.0\webapps\iforums-1.0-SNAPSHOT*
+cp target/iforums-1.0-SNAPSHOT.war D:\software\Tomcat-6.0\webapps\
+D:\software\Tomcat-6.0\bin\startup.bat
